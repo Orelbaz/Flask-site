9ec991756d0f7de2d2f9ff47ade302fb5f8db35c
Or <134198819+Orelbaz@users.noreply.github.com>
2023-07-02 10:28:59 +0300

    Add files via upload

---
+++ CoinSite/static/script.js
@@ -0,0 +1,9 @@
+<<<<<<< HEAD
+=======
+function showStockWorth(button) {
+    var StockWorth = button.parentNode.querySelector('.stock-worth');
+    netWorth.classList.toggle('hidden');
+
+    // Add additional effects here if desired
+}
+>>>>>>> 852aba495058c3dd4763b83ecb950d75ffb6d224
