e660ba64bbb4a85bba131becf1f10453792bbdfd
Or <134198819+Orelbaz@users.noreply.github.com>
2023-07-02 11:56:46 +0300

    Update deploy-compose.sh

--- deploy-compose.sh
+++ deploy-compose.sh
@@ -4,11 +4,12 @@ INSTANCE_IP=$1
 
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /var/lib/jenkins/or.pem ec2-user@${INSTANCE_IP} "
 sudo yum install docker -y
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 sudo systemctl enable docker
 sudo systemctl start docker
 sudo docker pull orelbaz/flak-docker:1.0
 sudo docker stop \$(sudo docker ps -aq)
 sudo docker rm \$(sudo docker ps -aq)
-cd /var/lib/jenkins/workspace/docker-compose-pipeline/flask-docker/CoinSite
 sudo docker-compose up
 "
