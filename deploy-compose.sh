08a75f13cfd3caaa3e1b0e9264118f80bebffcf4
Or <134198819+Orelbaz@users.noreply.github.com>
2023-07-02 12:42:39 +0300

    Update deploy-compose.sh

--- deploy-compose.sh
+++ deploy-compose.sh
@@ -8,7 +8,7 @@ sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 sudo systemctl enable docker
 sudo systemctl start docker
-sudo docker pull orelbaz/flak-docker:1.0
+#sudo docker pull orelbaz/flak-docker:1.0
 sudo docker stop \$(sudo docker ps -aq)
 sudo docker rm \$(sudo docker ps -aq)
 "
@@ -17,5 +17,5 @@ echo 'Copying docker-compose.yml to instance...'
 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /var/lib/jenkins/or.pem /var/lib/jenkins/workspace/docker-compose-pipeline/flask-docker/CoinSite/docker-compose.yml ec2-user@${INSTANCE_IP}:/home/ec2-user
 
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /var/lib/jenkins/or.pem ec2-user@${INSTANCE_IP} "
-cd /home/ec2-user
-sudo docker-compose up
+sudo docker pull orelbaz/flak-docker:1.0
+sudo docker-compose up -d
