eb067447c21d04dc978bc686a0da61047f2e19ea
Or <134198819+Orelbaz@users.noreply.github.com>
2023-07-02 13:38:09 +0300

    Update deploy-compose.sh

--- deploy-compose.sh
+++ deploy-compose.sh
@@ -2,23 +2,31 @@
 
 INSTANCE_IP=$1
 
+echo 'Copying docker-compose.yml to instance...'
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /var/lib/jenkins/or.pem /var/lib/jenkins/workspace/docker-compose-pipeline/flask-docker/CoinSite/docker-compose.yml ec2-user@${INSTANCE_IP}:/home/ec2-user
+
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /var/lib/jenkins/or.pem ec2-user@${INSTANCE_IP} "
 sudo yum install docker -y
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo systemctl enable docker
 sudo systemctl start docker
-#sudo docker pull orelbaz/flak-docker:1.0
+sudo docker pull orelbaz/flak-docker:1.0
 sudo docker stop \$(sudo docker ps -aq)
 sudo docker rm \$(sudo docker ps -aq)
-"
-
-echo 'Copying docker-compose.yml to instance...'
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /var/lib/jenkins/or.pem /var/lib/jenkins/workspace/docker-compose-pipeline/flask-docker/CoinSite/docker-compose.yml ec2-user@${INSTANCE_IP}:/home/ec2-user
-
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /var/lib/jenkins/or.pem ec2-user@${INSTANCE_IP} "
 pwd
 cd /home/ec2-user
 sudo docker pull orelbaz/flak-docker:1.0
 sudo docker-compose up -d
 "
+
+
+#echo 'Copying docker-compose.yml to instance...'
+#scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /var/lib/jenkins/or.pem /var/lib/jenkins/workspace/docker-compose-pipeline/flask-docker/CoinSite/docker-compose.yml ec2-user@${INSTANCE_IP}:/home/ec2-user
+
+#ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /var/lib/jenkins/or.pem ec2-user@${INSTANCE_IP} "
+#pwd
+#cd /home/ec2-user
+#sudo docker pull orelbaz/flak-docker:1.0
+#sudo docker-compose up -d
+#"
