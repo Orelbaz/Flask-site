7f2771478a6b84550cfab31c5e241450234fa041
Or <134198819+Orelbaz@users.noreply.github.com>
2023-07-02 14:51:57 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,14 +7,15 @@ scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /var/lib/jenk
 
 echo 'Connecting to test-server...'
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /var/lib/jenkins/or.pem ec2-user@${INSTANCE_IP} "
+sudo yum update -y
 sudo yum install docker -y
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo systemctl enable docker
 sudo systemctl start docker
-sudo docker pull orelbaz/coinsite:1.0
 sudo docker stop \$(sudo docker ps -aq)
 sudo docker rm \$(sudo docker ps -aq)
+sudo docker pull orelbaz/coinsite:1.0
 pwd
 cd /home/ec2-user
 sudo docker-compose up -d
