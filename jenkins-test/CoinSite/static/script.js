ae8d905ca18e58ea58e6c6543a8b43efac30520c
Or <134198819+Orelbaz@users.noreply.github.com>
2023-07-03 15:21:38 +0300

    Delete jenkins-test directory

--- jenkins-test/CoinSite/static/script.js
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-=======
-function showStockWorth(button) {
-    var StockWorth = button.parentNode.querySelector('.stock-worth');
-    netWorth.classList.toggle('hidden');
-
-    // Add additional effects here if desired
-}
->>>>>>> 852aba495058c3dd4763b83ecb950d75ffb6d224
